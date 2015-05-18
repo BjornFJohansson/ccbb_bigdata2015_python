bdc9571eb9d8b1077877a7fcf6f3ee89942cd19f
sjspielman <stephanie.spielman@utexas.edu>
2015-05-18 12:28:19 -0500

    update

--- slides/module_examples.py
+++ slides/module_examples.py
@@ -9,3 +9,28 @@ import sys
 #print sys.path
 #print sys.argv
 
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+seq = Seq("ACGTC")
+#print dir(seq)
+
+seqrec = SeqRecord(seq)
+
+#print seqrec
+
+# Defining a SeqRecord object
+#seq_object = Seq("ACGTC")
+#seq_record = SeqRecord(seq_object, id = "my_id", description="")
+
+#SeqIO.write(seq_record, "outfile.fasta", "fasta")
+
+
+recs = []
+for i in range(10):
+	seq_object = Seq("ACGTA")
+	seq_record = SeqRecord(seq_object, id = "id" + str(i))
+	recs.append(seq_record)
+
+SeqIO.write(recs, "outfile.fasta", "fasta")
